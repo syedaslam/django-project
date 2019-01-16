60826872821493a099033f0ee70e60d96c669f3b
syedaslam <s.aslam@blr.cordis>
2019-01-16 16:08:03 +0530

    add one more filed in model

--- src/languages/models.py
+++ src/languages/models.py
@@ -4,6 +4,7 @@ from django.db import models
 class Language(models.Model):
     name = models.CharField(max_length=50)
     disc = models.CharField(max_length=50)
+    summary = models.TextField()
 
     def __str__(self):
         return self.name
