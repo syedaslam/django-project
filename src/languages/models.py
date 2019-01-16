d2976d0d9f4e2d5de558887fa10edfa227ff9bd2
syedaslam <s.aslam@blr.cordis>
2019-01-16 16:12:49 +0530

    the previous changed have been modified

--- src/languages/models.py
+++ src/languages/models.py
@@ -4,7 +4,6 @@ from django.db import models
 class Language(models.Model):
     name = models.CharField(max_length=50)
     disc = models.CharField(max_length=50)
-    summary = models.TextField()
 
     def __str__(self):
         return self.name
